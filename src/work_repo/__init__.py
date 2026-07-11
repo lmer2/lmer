28aca113333d52944753d71dadcf176dbfefdada
Matt Griswold <grizz@20c.com>
2026-07-11 19:40:46 +0000

    chore: drop stale sub-package __version__ strings (never bumped, nothing imports them)

--- src/work_repo/__init__.py
+++ src/work_repo/__init__.py
@@ -5,7 +5,6 @@ from .info_reader import read_project_info
 from .git_ops import commit_work_changes
 from .utils import sanitize_task_target, redact_secrets
 
-__version__ = "0.1.0"
 __all__ = [
     "Logger",
     "YamlFileLogger",
